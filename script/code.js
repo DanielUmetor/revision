07130a4a75efa983992de9d6b79a91ff1ba2d2c2
DanielUmetor <dnumetor@gmail.com>
2024-05-21 16:30:12 +0200

    revision

---
+++ script/code.js
@@ -0,0 +1,131 @@
+/*
+-Functions
+-If statements
+-Switch/for statements
+-Query Selectors
+-Eval functions
+*/
+
+// Function: Is a set of statements to be utilized as needed.
+
+// Example 1=> function declaration
+
+// function display () {
+//    console.log("Hello there");
+// }
+// display ()
+// function greeting(firstName , lastName) {
+   // console.log(`My name is ${firstName} ${lastName}`)
+// }
+// Argument 
+// greeting('Daniel', 'Redding')
+// greeting('Ndamzi', 'Umetor')
+
+// Example 2 => return statement 
+
+/*
+Addition function to display the sum of two numbers
+*/
+/*
+function addition(numb1 , numb2) {
+    console.log(numb1 + numb2)
+}
+addition (7 , 3) 
+*/
+
+// function addition (...numbers) {
+  //  let digit = [];
+  //  numbers.forEach((value) => {
+    //    if (!isNaN(value)) {
+      //     digit.push(value)
+        //}
+   // })
+ 
+ //console.log(digit);
+ //console.log(digit.reduce((c, n) => c + n)); 
+// }
+  // addition('Sipho', 1,'Jamin', 2, 3,'Leah', 4, 2, 3, 1, 'Daniel')
+// console.log(isNaN('Joel'));
+
+ // sumOfValues(2, 1, 2)
+ //sumOfValues(2, 1, 2, 4)
+//  sumOfValues(2, 2, 2, 2)
+
+// function display(numb1, numb2) {
+ //   console.log(numb1, numb2);
+// }
+// display(2, 3) 
+
+
+// let numbers = [8, 9, 4, 2, 7]
+// numbers.forEach( (value, index) => {
+  //  console.log(`&{value} at position ${index}`);
+// }) 
+
+
+// difference between for, while, foreach, for in
+// for (let i = 0; i < 3; i++) {
+   //  console.log(i);             // for 
+// }
+  
+// let i = 0;                       // while
+// while (i < 3) {
+   //  console.log(i); 
+    // i++;
+
+
+// }
+// let array = [1, 2, 3, 4, 5];
+// array.forEach(function(element) {   //forEach
+   // console.log(element);
+// });
+
+/*
+FUNCTION EXPRESSION
+*/
+/*let addition = function () {   
+    console.log(3 + 3);
+}
+addition()  */
+// let multiplication = (numb1, numb2) => {
+   // console.log(numb1 * numb2);
+// }
+// multiplication (4, 3)
+
+
+/*
+FUNCTION DECLARATION
+*/ 
+// function greeting() {
+  //  console.log("Hello There");
+// }
+
+/* 
+HOISTING
+*/
+// function greeting(name) {
+//   console.log(`Bonjour ${name}`);
+// }
+// greeting('Daniel')
+// addition(3, 4)
+// let addition = (numb1, numb2) =>{
+  //  console.log(numb1 + numb2);
+// }
+let data = [9, 'Peter', 'Sipho', 3, 4, 11, 'Sarah']
+
+console.table(data) 
+
+// for (let i=0; i < data.length; i++) {   // data.length displays the size of the array
+ //   console.log(data[i]);
+// }
+
+// if you have an array(collection of data), make use of while loop
+
+// make use of forEach and display all names that start with 
+data.forEach((value) => {
+    
+})
+
+
+
+
